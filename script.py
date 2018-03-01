3affbff4b45c01b8870ad9c4ad24adb8f1e47e96
mark-mit-k <warneke.mark@gmail.com>
2018-03-01 22:13:37 +0100

    initial commit, basic DecisionTreeClassifier with sklearn

---
+++ script.py
@@ -0,0 +1,26 @@
+from sklearn import tree
+
+# [height, weight, shoe zise]
+X = [
+        [181, 80, 44],
+        [177, 70, 43],
+        [160, 60, 38],
+        [154, 54, 37],
+        [166, 65, 40],
+        [190, 90, 47],
+        [175, 64, 39],
+        [177, 70, 40],
+        [159, 55, 37],
+        [171, 75, 42],
+        [181, 85, 43]
+        ]
+# labels
+Y = ['male', 'female', 'female', 'female', 'male', 'male', 'male', 'female', 'male', 'female', 'male']
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X,Y)
+
+predict = [[190, 70, 43]]
+prediction = clf.predict(predict)
+
+print (prediction)
